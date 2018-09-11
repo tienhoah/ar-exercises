ddb175c2fa4589141c8e9945dde5ca93813f2044
tienhoah <6288799+tienhoah@users.noreply.github.com>
2018-09-11 21:48:46 +0000

    finish ex 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,12 +11,17 @@ puts "----------"
 # Your code goes here ...
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :store, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, :inclusion => 40..200
+
 end
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Joel", last_name: "Jo", hourly_rate: 51)
-@store2.employees.create(first_name: "Don", last_name: "Mon", hourly_rate: 30)
+@store2.employees.create(first_name: "Don", last_name: "Mon", hourly_rate: 120)
 @store2.employees.create(first_name: "Sam", last_name: "On", hourly_rate: 45)
 @store2.employees.create(first_name: "Jeremy", last_name: "Zzzz", hourly_rate: 80)
-@store2.employees.create(first_name: "Emma", last_name: "Lol", hourly_rate: 27)
+@store2.employees.create(first_name: "Emma", last_name: "Lol", hourly_rate: 47)
 puts
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "What is a store name?"
+storeinput = gets.chomp
+
+puts "what is annual revenue?"
+revenueinput = gets.chomp
+
+@store = Store.create(name: "#{storeinput}", annual_revenue: "#{revenueinput}")
+
+
+puts @store.errors[:name]
+puts @store.errors.details[:annual_revenue]
+puts @store.errors.to_a

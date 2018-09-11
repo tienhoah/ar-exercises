502f7cb37fb6ac7701287e9f10fddc8d9c7512b8
tienhoah <6288799+tienhoah@users.noreply.github.com>
2018-09-11 19:30:12 +0000

    Finish exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -11,3 +11,9 @@ Surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: fals
 Whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 Yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
+@mens_stores = Store.where(mens_apparel: true)
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue < ?", 1000000)
+
+@mens_stores.each{ |store| puts "#{store.name}: #{store.annual_revenue}" }
+@womens_stores.each{ |store| puts "#{store.name}: #{store.annual_revenue}" }
+puts
